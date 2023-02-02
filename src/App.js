b3e4981a074fa326beb4d890014faa84108005cf
701tkdgns <701tkdgns@inu.ac.kr>
2023-02-03 00:55:46 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const txtContent = useRef();
+    const handleKey = async (e) => {
+        if (e.key === "ArrowRight"){
+            // let txt = document.getElementsByClassName('txt').;
+            // console.log(txt);
+            txtContent.current.textContent = 'R';
+            console.log('right');
+        } else if (e.key === "ArrowUp"){
+            txtContent.current.textContent = 'U';
+            console.log('up')
+        } else if (e.key === "ArrowLeft"){
+            txtContent.current.textContent = 'L';
+            console.log('left')
+        } else if (e.key === "ArrowDown"){
+            txtContent.current.textContent = 'D';
+            console.log('down')
+        } else{
+            return;
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKey);
+
+        return () => {
+            window.removeEventListener("keydown", handleKey);
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Test</h1>
+            <div>
+                <p>In other word... <span className='txt' ref={txtContent}></span></p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
