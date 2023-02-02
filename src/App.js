6107225e9860d34f12636774aaf53d74d8d755f7
701tkdgns <701tkdgns@inu.ac.kr>
2023-02-03 00:58:04 +0900

    update annotation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useRef } from 'react'
 
 const App = () => {
+
+    // useRef : 리액트는 js DOM 직접 접근을 지양
+    // js dom element에 접근이 필요할 시 useRef 훅을 이용하여
+    // 값들을 저장해놨다가 사용 가능
+    // 렌더링시에도 초기화되지 않는 특성
+
     const txtContent = useRef();
     const handleKey = async (e) => {
         if (e.key === "ArrowRight"){
